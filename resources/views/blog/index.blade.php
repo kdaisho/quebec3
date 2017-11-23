1baeba7cb81f1ccb6e459e0e44f57b0d1a74c7e4
Daisho <daishokomiyama@gmail.com>
2017-11-22 21:55:05 -0500

    Style added to homepage

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,34 +4,70 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
-        <h1>Blog</h1>
-    </div>
+<div class="section is-blog">
+	<div class="container is-widescreen">
+		<h1 class="is-size-1">Blog</h1>
+	</div>
 </div>
 
-@foreach ($posts as $post)
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
-        @if($post->is_online)
-            <h2>{{ $post->title }}</h2>
-            <h5>Published: {{ date('M j, Y', strtotime($post->created_at)) }}</h5>
+<div class="section container is-widescreen">
+	<div class="columns">
+		<div class="column">
+			@foreach($posts->reverse() as $post)
+				@if($post->is_online)
+					<article class="media">
+						<figure class="media-left">
+							<p class="image is-64x64">
+								<img src="https://bulma.io/images/placeholders/128x128.png">
+							</p>
+						</figure>
 
-            <p>{!! mb_substr(strip_tags($post->body), 0, 250) !!}{{ mb_strlen($post->body) > 250 ? '...' : '' }}</p>
+						<div class="media-content">
+							<div class="content">
+								<h3>{{ $post->title }}</h3>
+								<h5>Published: {{ date('M j, Y', strtotime($post->created_at)) }}</h5>
+								<p>{!! mb_substr(strip_tags($post->body), 0, 200) !!}{{ mb_strlen($post->body) > 200 ? '...' : '' }}</p>
+							</div>
+						</div>
 
-            <a href="{{ route('blog.single', $post->slug) }}" class="btn btn-primary">Read More</a>
-            <hr>
-        @endif
-    </div>
+						<div class="media-right">
+							<a href="{{ route('blog.single', $post->slug) }}" class="button is-primary">Read More</a>
+						</div>
+
+					</article>
+				@endif
+			@endforeach
+		</div>
+	</div>
+
+	<div class="">
+		<div class="">ee
+			{!! $posts->links(); !!}
+		</div>
+	</div>
 </div>
-@endforeach
 
-<div class="row">
-    <div class="col-md-12">
-        <div class="text-center">
-            {!! $posts->links(); !!}
-        </div>
-    </div>
-</div>
 
+{{-- <div class="section container is-widescreen">
+	<div class="columns">
+		<div class="column">
+		@foreach ($posts as $post)
+			@if($post->is_online)
+				<h2>{{ $post->title }}</h2>
+				<h5>Published: {{ date('M j, Y', strtotime($post->created_at)) }}</h5>
+
+				<p>{!! mb_substr(strip_tags($post->body), 0, 250) !!}{{ mb_strlen($post->body) > 250 ? '...' : '' }}</p>
+
+				<a href="{{ route('blog.single', $post->slug) }}" class="btn btn-primary">Read More</a>
+				<hr>
+			@endif
+		@endforeach
+		</div>
+		<div class="column">
+			<div class="text-center">
+				{!! $posts->links(); !!}
+			</div>
+		</div>
+	</div>
+</div> --}}
 @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -7,7 +7,7 @@
 {{-- <div class="row"> --}}
 <div class="hero">
 	<div class="hero-body">
-		<div class="container">
+		<div class="container is-widescreen">
 			<h1 class="title">Welcome to my blog 3</h1>
 			<p class="subtitle">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or 
 			<p class="subtitle">
@@ -17,29 +17,84 @@
 	</div>
 </div>
 
-<div class="row">
-	<div class="col-md-8">
+<div class="section container" style="background: yellow;">
+	<div class="columns">
 
-		@foreach($posts as $post)
-			@if($post->is_online)
+		<div class="column is-9 is-12-mobile">
+			@foreach($posts as $post)
+				@if($post->is_online)
+					<article class="media">
+						<figure class="media-left">
+							<p class="image is-128x128">
+								<img src="https://bulma.io/images/placeholders/128x128.png">
+							</p>
+						</figure>
 
-				<div class="post">
-					<h3>{{ $post->title }}</h3>
-					<h3>{{ $post->is_online }}</h3>
-					<p>{!! mb_substr(strip_tags($post->body), 0, 200) !!}{{ mb_strlen($post->body) > 200 ? '...' : '' }}</p>
-					<a href="{{ route('blog.single', $post->slug) }}" class="btn btn-primary">Read More</a>
+						<div class="media-content">
+							<div class="content">
+								<h3>{{ $post->title }}</h3>
+								<p>{!! mb_substr(strip_tags($post->body), 0, 200) !!}{{ mb_strlen($post->body) > 200 ? '...' : '' }}</p>
+								<a href="{{ route('blog.single', $post->slug) }}" class="button is-primary">Read More</a>
+							</div>
+						</div>
+					</article>
+				@endif
+			@endforeach
+		</div>
+
+		<div class="column is-3 is-6-mobile">
+			<div class="card">
+				<div class="card-image">
+					<figure class="image is-4by3">
+						<img src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
+					</figure>
 				</div>
+				<div class="card-content">
+					<div class="media">
+						<div class="media-content">
+							<h3 class="title">This person writes.</h3>
+						</div>
+					</div>
+					<div class="content">
+						<p>blaf blah blah blaf blah blah blaf blah blah blaf blah blah blaf blah blah</p>
+						<a href="{{ url('about') }}" class="button is-primary">About Kyoshin</a>
+					</div>
+				</div>
+			</div>
+		</div>
 
-				<hr>
+	</div><!-- End of .columns -->
+</div>
 
-			@endif
+{{-- <div class="section container is-widescreen">
+	<div class="columns">
+		@foreach($posts as $post)
+		@if($post->is_online)
+		<div class="column is-narro" style="flex-grow: 0;">
+			<div class="card" style="max-width: 100%; width: 300px;">
+				<div class="card-image">
+					<figure class="image is-4by3">
+						<img src="https://bulma.io/images/placeholders/128x128.png" alt="Placeholder image">
+					</figure>
+				</div>
+				<div class="card-content">
+					<div class="media">
+						<div class="media-content">
+							<h3 class="title is-4">{{ $post->title }}</h3>
+						</div>
+					</div>
+					<div class="content">
+						<p>{!! mb_substr(strip_tags($post->body), 0, 100) !!}{{ mb_strlen($post->body) > 100 ? '...' : '' }}</p>
+						<a href="{{ route('blog.single', $post->slug) }}" class="button is-primary">Read More</a>
+						<br>
+						<time>{{ $post->created_at }}</time>
+					</div>
+				</div>
+			</div>
+		</div>
+		@endif
 		@endforeach
 	</div>
-	<div class="col-md-3 col-md-offset-1">
-		<div class="post">
-			<h2>Sidebar</h2>
-		</div>
-	</div>
-</div>
+</div> --}}
 
 @endsection

f6bf1f089661adc79b60689d96b13a3edac0e506
Daisho <daishokomiyama@gmail.com>
2017-12-04 23:23:02 -0500

    changed image path

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -16,7 +16,8 @@
 <section class="section">
 	<div class="container columns">
 		<div class="column is-8 is-offset-2">
-			<img class="featured-image" src="{{ asset('images/' . $post->image) }}-original.jpg" alt="Featured Image: {{ $post->title }}">
+			{{-- <img class="featured-image" src="{{ asset('images/' . $post->image) }}-original.jpg" alt="Featured Image: {{ $post->title }}"> --}}
+			<img class="featured-image" src="{{ asset('/images/' . $post->image) }}-original.jpg" alt="Featured Image: {{ $post->title }}">
 			<div class="has-text-centered">
 				<h1 class="is-size-1 is-size-3-mobile m-t-20">{{ $post->title }}</h1>
 				<p class="has-text-weight-light m-b-10">{{ date('Y年 m月d日 g:i A',  strtotime($post->created_at)) }}</p>
@@ -26,7 +27,6 @@
 				@endforeach
 			</div>
 
-			{{-- <div class="column is-desktop m-t-30 is-8 is-offset-2"> --}}
 			<div class="is-desktop m-t-30">
 				{!! $post->body !!}
 			</div>

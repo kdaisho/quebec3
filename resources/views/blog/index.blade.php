39e2632e9751562da362934c439d30d0908850f9
Daisho <daishokomiyama@gmail.com>
2017-12-09 18:20:55 -0500

    Set redirect (on nginx), added style to blog.index and welcome

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,57 +4,62 @@
 
 @section('content')
 
-<div class="section is-bg-purple has-text-centered">
-	<div class="container is-widescreen">
-		<h1 class="title is-size-1 is-size-3-mobile has-text-white">ブログ一覧</h1>
-		<p class="has-text-white has-text-centered is-size-5 is-size-6-mobile text-bg">new! 記事にコメントを残せるようになりました。<span class="no-wrap">熱心な読者様向けに</span><span class="no-wrap">新着記事通知サービスも</span>やってます。</p>
+<section class="is-blog-index">
+
+	<div class="section is-bg-purple has-text-centered">
+		<div class="container is-widescreen">
+			<h1 class="title is-size-1 is-size-3-mobile has-text-white">ブログ一覧</h1>
+			<p class="has-text-white has-text-centered is-size-5 is-size-6-mobile text-bg">new! 記事にコメントを残せるようになりました。<span class="no-wrap">熱心な読者様向けに</span><span class="no-wrap">新着記事通知サービスも</span>やってます。</p>
+		</div>
 	</div>
-</div>
 
-<div class="section container is-widescreen">
-	<div class="columns">
-		<div class="column is-9 is-12-mobile m-b-30">
-		@foreach($posts as $post)
-			@if($post->is_online)
+	<div class="section container is-widescreen">
+		<div class="columns">
+			<div class="column is-9 is-12-mobile m-b-30">
+			@foreach($posts as $post)
+				@if($post->is_online)
 
-				@if($post->id % 4 == 0 )
-					{{-- in-feed ads --}}
-					{{-- @include('partials._ads-infeed') --}}
-				@endif
+					@if($post->id % 4 == 0 )
+						{{-- in-feed ads --}}
+						{{-- @include('partials._ads-infeed') --}}
+					@endif
 
-				<article class="media">
-					<figure class="media-left">
-						<p class="image is-128x128 m-b-15">
-							@if(isset($post->image))
-								<img src="images/{{ $post->slug . '/' . $post->image }}-thumb.jpg" alt="Featured image: {{ $post->title }}">
-							@else
-								<img src="http://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_nikkor28-300mmf_35-56gd_ed_vr/img/sample/sample4_l.jpg" alt="featured image: general">
-							@endif
-						</p>
-					</figure>
+					<article class="media">
+						<figure class="media-left">
+							{{-- <p class="image is-128x128 m-b-15"> --}}
+							<p class="image is-64x64 m-b-15">
+								@if(isset($post->image))
+									<img src="images/{{ $post->slug . '/' . $post->image }}-thumb.jpg" alt="Featured image: {{ $post->title }}">
+								@else
+									<img src="http://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_nikkor28-300mmf_35-56gd_ed_vr/img/sample/sample4_l.jpg" alt="featured image: general">
+								@endif
+							</p>
+						</figure>
 
-					<div class="media-content">
-						<div class="content">
-							<h3 class="m-b-5">{{ $post->title }}</h3>
-							<p class="has-text-weight-light is-size-6 m-b-5">{{ date('Y年 m月d日',  strtotime($post->created_at)) }}</p>
-							<p>{!! mb_substr(strip_tags($post->body), 0, 200) !!}{{ mb_strlen($post->body) > 200 ? '...' : '' }}</p>
-							<a href="{{ route('blog.single', $post->slug) }}" class="button is-primary">続きを読む</a>
+						<div class="media-content">
+							<div class="content">
+								<h3 class="m-b-5 is-size-4 is-size-5-mobile">{{ $post->title }}</h3>
+								<p class="has-text-weight-light is-size-6 is-size-7-mobile m-b-5">{{ date('Y年 m月d日',  strtotime($post->created_at)) }}</p>
+								<p>{!! mb_substr(strip_tags($post->body), 0, 200) !!}{{ mb_strlen($post->body) > 200 ? '...' : '' }}</p>
+								<a href="{{ route('blog.single', $post->slug) }}" class="button is-primary">続きを読む</a>
+							</div>
 						</div>
-					</div>
-				</article>
-			@endif
-		@endforeach
-		</div>
+					</article>
+				@endif
+			@endforeach
+			</div>
 
-		<div class="column">
-			{{-- Signup Form --}}
-			@include('partials.signup')
+			<div class="column">
+				{{-- Signup Form --}}
+				@include('partials.signup')
+			</div>
 		</div>
 	</div>
-</div>
 
-<nav class="pagination" role="navigation" aria-label="pagination">
-	{{ $posts->links() }}
-</nav>
+	<nav class="pagination" role="navigation" aria-label="pagination">
+		{{ $posts->links() }}
+	</nav>
+
+</section>
 
 @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -4,99 +4,101 @@
 
 @section('content')
 
-<div class="hero">
-	<div class="hero-body">
-		<div class="container is-widescreen has-text-centered">
-			<h1 class="has-text-white title is-size-1 m-b-30">Quebec3</h1>
-			<p class="subtitle has-text-white">- 海外移住ポータル -</p>
-			<a class="button is-primary is-medium" href="{{ route('blog.index') }}" role="button">記事一覧</a>
+<section class="is-welcome">
+
+	<div class="hero">
+		<div class="hero-body">
+			<div class="container is-widescreen has-text-centered">
+				<h1 class="has-text-white title is-size-1 m-b-30">Quebec3</h1>
+				<p class="subtitle has-text-white">- 海外移住ポータル -</p>
+				<a class="button is-primary is-medium" href="{{ route('blog.index') }}" role="button">記事一覧</a>
+			</div>
 		</div>
 	</div>
-</div>
 
-<div class="is-size-6 column is-6-desktop is-offset-3-desktop is-10-tablet is-offset-1-tablet has-text-centered m-t-10">
-	<p>
-		Quebec3は、モントリオール在住のとある日本人により運営されている気ままな個人のブログです。モントリオール/ケベック州での生活ぶりを勝手な視点から書いています。
-	</p>
-	<p>
-		このブログには将来海外に住んでみたいと思っている人、留学を考えている人、または日本の労働環境があまりに劣悪なため日本脱出を考えている人のモチベーションを向上させる効果があります。
-	</p>
-	<p>
-		これまで日本だけでずーっと過ごして来た人、英語を勉強している人、ポテチの袋が上手に開けられない人、皆で仲良くQuebec3を読んでカナダ移住を追体験してください。
-	</p>
-</div>
+	<div class="is-size-6 column is-6-desktop is-offset-3-desktop is-10-tablet is-offset-1-tablet has-text-centered m-t-10">
+		<p>
+			Quebec3は、モントリオール在住のとある日本人により運営されている気ままな個人のブログです。モントリオール/ケベック州での生活ぶりを勝手な視点から書いています。
+		</p>
+		<p>
+			このブログには将来海外に住んでみたいと思っている人、留学を考えている人、または日本の労働環境があまりに劣悪なため日本脱出を考えている人のモチベーションを向上させる効果があります。
+		</p>
+		<p>
+			これまで日本だけでずーっと過ごして来た人、英語を勉強している人、ポテチの袋が上手に開けられない人、皆で仲良くQuebec3を読んでカナダ移住を追体験してください。
+		</p>
+	</div>
 
-<div class="section container">
-	
-	<h2 class="title is-size-2 is-size-3-mobile has-text-centered m-b-50">最近の記事</h2>
-	<div class="columns">
-		<div class="column is-12-mobile m-b-30">
-			@foreach($posts as $post)
-				@if($post->is_online)
+	<div class="section container">
 
-					@if($post->id % 4 == 0 )
-						{{-- in-feed ads --}}
-						{{-- @include('partials._ads-infeed') --}}
+		<h2 class="title is-size-2 is-size-3-mobile has-text-centered m-b-50">最近の記事</h2>
+		<div class="columns">
+			<div class="column is-12-mobile m-b-30">
+				@foreach($posts as $post)
+					@if($post->is_online)
+
+						@if($post->id % 4 == 0 )
+							{{-- in-feed ads --}}
+							{{-- @include('partials._ads-infeed') --}}
+						@endif
+
+						<article class="media">
+							<figure class="media-left">
+								<span class="image is-64x64">
+									@if(isset($post->image))
+										<img src="/images/{{ $post->slug . '/' . $post->image }}-thumb.jpg" alt="Featured image: {{ $post->title }}">
+									@else
+										<img src="/images/profile.jpg" alt="featured image: general">
+									@endif
+								</span>
+							</figure>
+
+							<div class="media-content">
+								<div class="content">
+									<h3 class="m-b-5 is-size-4 is-size-5-mobile">{{ $post->title }}</h3>
+									<p class="m-b-5 has-text-weight-light is-size-6">{{ date('Y年 m月d日',  strtotime($post->created_at)) }}</p>
+									<p>{!! mb_substr(strip_tags($post->body), 0, 200) !!}{{ mb_strlen($post->body) > 200 ? '...' : '' }}</p>
+									<a href="{{ route('blog.single', $post->slug) }}" class="button is-primary">続きを読む</a>
+								</div>
+							</div>
+						</article>
 					@endif
+				@endforeach
 
-					<article class="media">
-						<figure class="media-left">
-							<span class="image is-128x128">
-								@if(isset($post->image))
-									<img src="/images/{{ $post->slug . '/' . $post->image }}-thumb.jpg" alt="Featured image: {{ $post->title }}">
-								@else
-									<img src="/images/profile.jpg" alt="featured image: general">
-								@endif
-							</span>
+				<hr>
+				<a class="button is-primary is-fullwidth m-t-30" href="{{ route('blog.index') }}" role="button">記事一覧へ</a>
+			</div>
+
+			<div class="column is-3" style="max-width: 256px;">
+				<div class="card" >
+					<div class="card-image">
+						<figure class="image is-256x256">
+							<img src="/images/profile-2.jpg" alt="Placeholder image">
 						</figure>
-
-						<div class="media-content">
-							<div class="content">
-								<h3 class="is-size-3 is-size-4-mobile">{{ $post->title }}</h3>
-								<small class="has-text-weight-light">{{ date('Y年 m月d日',  strtotime($post->created_at)) }}</small>
-								<p>{!! mb_substr(strip_tags($post->body), 0, 200) !!}{{ mb_strlen($post->body) > 200 ? '...' : '' }}</p>
-								<a href="{{ route('blog.single', $post->slug) }}" class="button is-primary">続きを読む</a>
+					</div>
+					<div class="card-content">
+						<div class="media">
+							<div class="media-content">
+								<h2 class="title is-size-5">この人が<br>書いてます。</h2>
 							</div>
 						</div>
-					</article>
-				@endif
-			@endforeach
-
-			<hr>
-			<a class="button is-primary is-fullwidth m-t-30" href="{{ route('blog.index') }}" role="button">記事一覧へ</a>
-		</div>
-
-		<div class="column is-3" style="max-width: 256px;">
-			<div class="card" >
-				<div class="card-image">
-					<figure class="image is-256x256">
-						<img src="/images/profile-2.jpg" alt="Placeholder image">
-					</figure>
-				</div>
-				<div class="card-content">
-					<div class="media">
-						<div class="media-content">
-							<h2 class="title is-size-5">この人が<br>書いてます。</h2>
+						<div class="content">
+							<h3 class="is-size-4">Kyoshin</h3>
+							<p>2010年より<span class="no-wrap">モントリオール</span>在住の<span class="no-wrap">カナダ市民権</span><span class="no-wrap">保持者。</span><br>元陸自の<span class="no-wrap">Web屋</span><span class="no-wrap">さん。</span><br>カレー好き。</p>
+							<a href="{{ url('about') }}" class="button is-primary">詳しく見る</a>
 						</div>
-					</div>
-					<div class="content">
-						<h3 class="is-size-4">Kyoshin</h3>
-						<p>2010年より<span class="no-wrap">モントリオール</span>在住の<span class="no-wrap">カナダ市民権</span><span class="no-wrap">保持者。</span><br>元陸自の<span class="no-wrap">Web屋</span><span class="no-wrap">さん。</span><br>カレー好き。</p>
-						<a href="{{ url('about') }}" class="button is-primary">詳しく見る</a>
 					</div>
 				</div>
 			</div>
-		</div>
-		
-	</div><!-- End of .columns -->
 
-	<hr>
+		</div><!-- End of .columns -->
 
-	<div class="m-t-50">
-		<div class="column is-6-desktop is-offset-3-desktop is-10-tablet is-offset-1-tablet">
-			@include('partials._form')
+		<hr>
+
+		<div class="m-t-50">
+			<div class="column is-6-desktop is-offset-3-desktop is-10-tablet is-offset-1-tablet">
+				@include('partials._form')
+			</div>
 		</div>
 	</div>
-</div>
-
+</section>
 @endsection

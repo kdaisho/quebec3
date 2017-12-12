cc50b2357cc6e5faaefe46818a8bf9669e9c8433
Daisho <daishokomiyama@gmail.com>
2017-12-12 08:14:25 -0500

    Changed posts order in blog index page

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -8,7 +8,7 @@ use App\Post;
 class BlogController extends Controller
 {
 	public function getIndex() {
-		$posts = Post::orderBy('created_at', 'desc')->paginate(10);
+		$posts = Post::orderBy('id', 'desc')->paginate(10);
 		return view('blog.index')->with('posts', $posts);
 	}
 

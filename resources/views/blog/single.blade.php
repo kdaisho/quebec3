c220c7002b3215432cbd8b0c5476ac04e642f2e4
Daisho <daishokomiyama@gmail.com>
2017-12-25 14:41:54 -0500

    Changed css video class to iframe as it is applied to not only video iframe, JS adsense insertion fixed

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -70,8 +70,8 @@
 <script>
 /* === adsense insertion === */
 (function() {
-	document.getElementById("ads1").innerHTML = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-3483277501004098" data-ad-slot="3797668697"></ins> <script>(adsbygoogle = window.adsbygoogle || []).push({});<\/script>';
-	document.getElementById("ads2").innerHTML = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-3483277501004098" data-ad-slot="3374302335"></ins> <script>(adsbygoogle = window.adsbygoogle || []).push({});<\/script>';
+	document.getElementById("ads1") ? document.getElementById("ads1").innerHTML = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-3483277501004098" data-ad-slot="3797668697"></ins> <script>(adsbygoogle = window.adsbygoogle || []).push({});<\/script>' : '';
+	document.getElementById("ads2") ? document.getElementById("ads2").innerHTML = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"><\/script><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-3483277501004098" data-ad-slot="3374302335"></ins> <script>(adsbygoogle = window.adsbygoogle || []).push({});<\/script>' : '';
 
 	var ads = document.getElementsByClassName("ads");
 	if(ads[0]) {

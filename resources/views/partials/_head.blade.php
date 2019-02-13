1de028d42418bdf636d29397c91789eb732b9151
Daisho <daishokomiyama@gmail.com>
2019-02-13 17:38:16 -0500

    Updating meta tag and homepage

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <title>Quebec3 - 海外移住ポータル@yield('title')</title>
-<meta name="description" content="海外移住, カナダ移住, ケベック州, モントリオール, 英会話, ブログ, @yield('desc')フランス語">
+<meta name="description" content="Quebec3は、モントリオール在住のとある日本人により運営されている気ままな個人のブログです。モントリオール/ケベック州での生活ぶりを勝手な視点から書いています。このブログには将来海外に住んでみたいと思っている人、留学を考えている人、または日本の労働環境があまりに劣悪なため日本脱出を考えている人のモチベーションを上げる効果があります。これまで日本だけでずーっと過ごして来た人、英語を勉強している人、日本国籍を捨ててみたい人、外国に移住したい人、海外でエンジニアになりたい人、皆で仲良くQuebec3を読んでカナダ移住を追体験してください。海外移住での生活情報、カナダ移住、ケベック州とは、モントリオールでの生活、英語や英会話、海外移住者ブログ、カナダでの離婚手続き、日本国籍の捨て方、海外でエンジニアやモントリオールでのフランス語などについて情報発信しています。">
 <!-- FB open graph tags-->
 @yield('openGraph')
 <link rel="canonical" href="{{ url()->current() }}" />
